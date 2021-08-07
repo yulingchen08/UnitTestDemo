f1f102c47b737c25df2074f90e7c753b79aab66b
Yuling <yulingchen08@gmail.com>
2021-08-07 10:18:47 +0800

    Initial Commit

---
+++ Shared/UnitTestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestDemoApp.swift
+//  Shared
+//
+//  Created by 陳鈺翎 on 2021/8/7.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
